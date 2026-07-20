ca352abcfc457d7e779af95efff23819da07822e
tamehar560-rgb <tamehar560@gmail.com>
2026-07-20 12:39:09 +0500

    Add birthday cake click event and wish function

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cake = document.querySelector(".cake");
+
+    cake.addEventListener("click", function () {
+        alert("🎂 Happy Birthday! May your day be filled with happiness and smiles ❤️");
+    });
+});
+function showWish() {
+    alert("🎂 Happy Birthday! May your day be full of happiness, success and beautiful moments ❤️");
+}
